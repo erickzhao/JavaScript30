57ee921bb92a052e69a1ad50aed70925bbfcacab
erick <erick@hotmail.ca>
2016-12-17 00:20:35 -0500

    Add sound for single button press

---
+++ day1/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('keydown', function(e) {
+
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+
+    if (!audio){
+        return ;
+    }
+    
+    audio.currentTime = 0; //rewind sound
+    audio.play(); //play
+});
