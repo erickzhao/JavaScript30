c2b08f8ba199ca6a96d88ac1531d1a44daf12d87
erick <erick@hotmail.ca>
2016-12-17 22:22:17 -0500

    Create base functionality with Web Audio API

--- day1/script.js
+++ day1/script.js
@@ -1,12 +1,103 @@
-window.addEventListener('keydown', function(e) {
+window.onload = init;
+var context;
+var bufferLoader;
 
+/*************************************************************************************
+    BufferLoader class taken from
+    https://www.html5rocks.com/en/tutorials/webaudio/intro/js/buffer-loader.js
+*************************************************************************************/
 
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+function BufferLoader(context, urlList, callback) {
+    this.context = context;
+    this.urlList = urlList;
+    this.onload = callback;
+    this.bufferList = new Array();
+    this.loadCount = 0;
+}
 
-    if (!audio){
-        return ;
+BufferLoader.prototype.loadBuffer = function(url, index) {
+    // Load buffer asynchronously
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    var loader = this;
+
+    request.onload = function() {
+        // Asynchronously decode the audio file data in request.response
+        loader.context.decodeAudioData(request.response,
+            function(buffer) {
+                if (!buffer) {
+                    alert('error decoding file data: ' + url);
+                    return;
+                }
+                loader.bufferList[index] = buffer;
+
+                if (++loader.loadCount == loader.urlList.length){
+                    loader.onload(loader.bufferList);
+                }
+            },
+            function(error) {
+                console.error('decodeAudioData error', error);
+            }
+        );
     }
-    
-    audio.currentTime = 0; //rewind sound
-    audio.play(); //play
-});
+
+    request.onerror = function() {
+        alert('BufferLoader: XHR error');
+    }
+
+    request.send();
+}
+
+BufferLoader.prototype.load = function() {
+      for (var i = 0; i < this.urlList.length; ++i)
+      this.loadBuffer(this.urlList[i], i);
+}
+
+/*************************************************************************************
+*************************************************************************************/
+
+function init() {
+    // Fix up prefixing
+    context = new (window.AudioContext || window.webkitAudioContext)();
+
+    bufferLoader = new BufferLoader(
+        context,
+        [
+          'sounds/bass.wav',
+          'sounds/kick.wav',
+          'sounds/clap.wav',
+          'sounds/snap.wav',
+          'sounds/hi-hat.wav',
+          'sounds/snare.wav',
+          'sounds/tom.wav',
+          'sounds/rim.wav',
+          'sounds/damn.mp3'
+        ],
+        setKeys
+    );
+
+    bufferLoader.load();
+}
+
+function setKeys(bufferList) {
+
+    window.addEventListener('keydown', function(e){
+
+        var source = context.createBufferSource();
+
+        source.connect(context.destination);
+
+        //key codes for [Q,W,E,A,S,D,Z,X,C]
+        var codes = [81,87,69,65,83,68,90,88,67];
+
+        codes.forEach( function(code, index) {
+            if (e.keyCode == code) {
+                source.buffer = bufferList[index];
+            }
+        });
+
+        source.start(0);
+    });
+}
